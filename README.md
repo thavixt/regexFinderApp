81fdd7f23c3ad75cd758aee5484615e56735af9f
Peter Komlosi <komlosi.peti@gmail.com>
2017-06-08 04:05:11 +0200

    theme changer, input validaiton, bacon ipsum

--- README.md
+++ README.md
@@ -1,11 +1,33 @@
 # regexFinderApp
-Find the RegEx results in your text.
+A simple Regular Expression tester app.
 
 ## Usage
-TODO
+1. Enter a RegEx pattern
+2. Choose from the RegEx flags
+3. Copy&paste or generate some text
+4. Press the button!
+
+## Features
+* Optional RegEx flags
+	* Global search (g)
+	* Case-insensitive (c)
+	* Multi-line search(m)
+	* Unicode (u)
+* Generate *Bacon ipsum* text
+* Switch between light and dark themes
+* Display matches in the provided text
+
+## TODO
+- [x] toggle RegEx flags
+- [x] Light/dark theme switcher
+	* Improve themes
+- [] RegEx examples
+- [] RegEx helper tables / infoboxes
+- [] Hover tooltip boxes on results text (match #, etc)
+- [x] Generate *Bacon ipsum* text
 
 ## Credits
-TODO
+* [Bacon Ipsum](https://baconipsum.com/json-api/)
 
 ## License
-TODO
+todo
--- js/main.js
+++ js/main.js
@@ -1,40 +1,64 @@
 // form
-const rgxForm = document.getElementById('rgx-form');
 const rgxInput = document.getElementById('rgx-input');
-const rgxInfo = document.getElementById('rgx-info');
-const textInput = document.getElementById('rgx-text');
+const rgxInfo = document.getElementById('rgx-input-info');
+const textInput = document.getElementById('text');
+const textInfo = document.getElementById('text-info');
 // arguments
 const rgxArgGlobal = document.getElementById('rgx-arg-g');
 const rgxArgCase = document.getElementById('rgx-arg-i');
 const rgxArgMultiLine = document.getElementById('rgx-arg-m');
 const rgxArgUnicode = document.getElementById('rgx-arg-u');
-// result
+// buttons
+const executeBtn = document.getElementById('rgx-submit');
+const changeThemeBtn = document.getElementById('change-theme-btn');
+// bacon
+var getBaconBtn = document.getElementById('text-bacon');
+const setBaconLen = document.getElementById('text-bacon-len');
+const baconLenNum = document.getElementById('text-bacon-len-num');
+// result area
 const resRgx = document.getElementById('result-rgx');
 const resNum = document.getElementById('result-num');
 const resDiv = document.getElementById('result-text');
+
+// variables
+var themeHref = document.getElementById('theme');
+var theme = true;
+var bacon = setBaconLen.value;
 
 // indicators for the result text
 var iStart = "<strong>";
 var iEnd = "</strong>";
 
-rgxForm.addEventListener('submit', testRegEx);
-
 function testRegEx(e) {
 	e.preventDefault();
 	
-	// prevent infinite loop
+	// if pattern is empty
 	if (rgxInput.value == '') {
 		rgxInput.classList.add("warning");
-		rgxInfo.innerHTML = 'invalid expression';
+		rgxInfo.innerHTML = 'invalid expression!';
 		resRgx.innerHTML = '';
 		resDiv.innerHTML = '';
+		console.log("empty input");
 		return false;
 	}
+	// if textarea is empty
+	if (textInput.value == "") {
+		textInput.classList.add("warning");
+		textInfo.innerHTML = 'textarea is empty!';
+		resRgx.innerHTML = '';
+		resDiv.innerHTML = '';
+		console.log("empty textarea");
+		return false;
+	}
+	// else clear warnings
 	rgxInput.classList.remove("warning");
 	rgxInfo.innerHTML = '';
+	textInput.classList.remove("warning");
+	textInfo.innerHTML = '';
 	
 	// gather checked flags
-	var args = "g";
+	var args = '';
+	if (rgxArgGlobal.checked) { args += 'g' }
 	if (rgxArgCase.checked) { args += 'i' }
 	if (rgxArgMultiLine.checked) { args += 'm' }
 	if (rgxArgUnicode.checked) { args += 'u' }
@@ -47,31 +71,78 @@ function testRegEx(e) {
 	var t = textInput.value;
 	var result = t;
 
-	var o = 0; // offest
-	var c = 0; // result counter
-	while ((match = r.exec(t)) != null) {
-		c++;
-		//console.log("match found at " + match.index +': '+ t.substring(match.index, match.index+match[0].length));
-		result = result.insert(o + match.index, 0, iStart); o+=8;
-		result = result.insert((o + match.index + match[0].length), 0, iEnd); o+=9;
+	if (!rgxArgGlobal.checked) {
+
+		resNum.innerHTML = '- 1 match';
+		return true;
+	}
+	else {
+		var o = 0; // offest
+		var c = 0; // result counter
+		while ((match = r.exec(t)) != null) {
+			c++;
+			//console.log("match found at " + match.index +': '+ t.substring(match.index, match.index+match[0].length));
+			result = result.insert(o + match.index, 0, iStart); o+=8;
+			result = result.insert((o + match.index + match[0].length), 0, iEnd); o+=9;
+		}
+		
+		if(c==0) {
+			resNum.innerHTML = ' - no matches';
+		} else if (c==1) {
+			resNum.innerHTML = ' - ' + c + ' match';
+		} else {
+			resNum.innerHTML = ' - ' + c + ' matches';
+		}
+		
+		resRgx.innerHTML = r;
+		resDiv.innerHTML = result;
+		return true;
 	}
 	
-	if(c==0) {
-		resNum.innerHTML = ' - no matches';
-	} else if (c==1) {
-		resNum.innerHTML = ' - ' + c + ' match';
-	} else {
-		resNum.innerHTML = ' - ' + c + ' matches';
-	}
-	resRgx.innerHTML = r;
-	resDiv.innerHTML = result;
-	return true;
 }
 
 String.prototype.insert = function(idx, rem, str) {
 	return this.slice(0, idx) + str + this.slice(idx + Math.abs(rem));
 };
 
+// put Bacon Ipsum into the textarea
+function getBacon(e) {
+	e.preventDefault();
+	getBaconBtn.innerHTML = "serving bacon ... ";
+	// create reader to get text from the response blob
+	var reader = new FileReader();
+	reader.onload = function() {
+		getBaconBtn.innerHTML = "Give me a new bacon";
+		textInput.value = reader.result;
+	}
+	// fetch paragraphs
+	fetch('https://baconipsum.com/api/?type=meat-and-filler&start-with-lorem=1&paras='+bacon+'&format=text')
+	.then(response => response.blob())
+	.then(blob => reader.readAsText(blob))
+}
+function setBaconLength() {
+	bacon = setBaconLen.value;
+	if (bacon > 1) baconLenNum.innerHTML = bacon + " bacons";
+	else { baconLenNum.innerHTML = bacon + " bacons"; }
+}
+
+function changeTheme() {
+	if(theme) {
+		themeHref.href = 'css/dark.css';
+		theme = false;
+	}
+	else {
+		themeHref.href = 'css/light.css';
+		theme = true;
+	}
+	console.log('theme changed');
+}
+
+// event listeners
+executeBtn.addEventListener('click', testRegEx);
+getBaconBtn.addEventListener('click', getBacon);
+setBaconLen.addEventListener('input', setBaconLength);
+changeThemeBtn.addEventListener('click', changeTheme);
 
 /*
 // test in console:
@@ -83,6 +154,5 @@ while ((match = re.exec(str)) != null) {
 */
 
 (function(){
-	textInput.value = 'Nunc eget est porta, aliquam orci ac, venenatis nisi. Pellentesque sodales volutpat pharetra. Pellentesque cursus nisl eget dui volutpat dignissim. Integer eu efficitur ex. Nullam quis nunc ac urna scelerisque iaculis. Fusce neque augue, blandit ac felis ac, semper fermentum nunc. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In non orci a nisl placerat viverra. Duis nunc purus, auctor in est nec, cursus tempus quam. In suscipit luctus congue.\r\n\nContrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.\r\n\nThe standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.\r\n\n- http://www.lipsum.com/';
-	rgxInput.value = 'lorem';
+	setBaconLength();
 })();
