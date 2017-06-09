9f205817878e082023b2f313edf5685765bbe379
Peter Komlosi <komlosi.peti@gmail.com>
2017-06-09 05:47:57 +0200

    improved themes, added expert mode and js cookies

--- README.md
+++ README.md
@@ -3,31 +3,36 @@ A simple Regular Expression tester app.
 
 ## Usage
 1. Enter a RegEx pattern
-2. Choose from the RegEx flags
-3. Copy&paste or generate some text
-4. Press the button!
+2. Choose from the provided RegEx flags
+3. Copy&Paste or generate some text
+4. Press the *Execute* button
 
 ## Features
+* Practice Regular Expressions
 * Optional RegEx flags
-	* Global search (g)
+	* Global (g)
 	* Case-insensitive (c)
-	* Multi-line search(m)
+	* Multi-line search (m)
 	* Unicode (u)
-* Generate *Bacon ipsum* text
+* Generate *Bacon ipsum* and *Expert* text
 * Switch between light and dark themes
-* Display matches in the provided text
+* Display matches in the text
 
 ## TODO
 - [x] toggle RegEx flags
+- [x] Generate *Bacon ipsum* text
+- [x] *Expert mode* - raw text from a random Wikipedia article
+- [x] Handle global/no-global searches
 - [x] Light/dark theme switcher
-	* Improve themes
+- [x] Save and load preferences from JS cookies
 - [] RegEx examples
 - [] RegEx helper tables / infoboxes
 - [] Hover tooltip boxes on results text (match #, etc)
-- [x] Generate *Bacon ipsum* text
 
 ## Credits
 * [Bacon Ipsum](https://baconipsum.com/json-api/)
+* [Wikipedia API](https://www.mediawiki.org/wiki/API:Query)
+* [CORS anywhere](https://cors-anywhere.herokuapp.com/)
 
 ## License
 todo
--- js/main.js
+++ js/main.js
@@ -11,24 +11,32 @@ const rgxArgUnicode = document.getElementById('rgx-arg-u');
 // buttons
 const executeBtn = document.getElementById('rgx-submit');
 const changeThemeBtn = document.getElementById('change-theme-btn');
-// bacon
-var getBaconBtn = document.getElementById('text-bacon');
-const setBaconLen = document.getElementById('text-bacon-len');
-const baconLenNum = document.getElementById('text-bacon-len-num');
+// text api
+const getExpertTextBtn = document.getElementById('text-expert-get');
+const getTextBtn = document.getElementById('text-get');
+const setTextLen = document.getElementById('text-set-len');
+const inputTextLen = document.getElementById('text-len-num');
 // result area
 const resRgx = document.getElementById('result-rgx');
 const resNum = document.getElementById('result-num');
 const resDiv = document.getElementById('result-text');
 
+// proxy to bypass CORS errors
+const corsProxy = 'https://cors-anywhere.herokuapp.com/';
+
 // variables
 var themeHref = document.getElementById('theme');
-var theme = true;
-var bacon = setBaconLen.value;
 
 // indicators for the result text
 var iStart = "<strong>";
 var iEnd = "</strong>";
 
+// insert into string
+String.prototype.insert = function(idx, rem, str) {
+	return this.slice(0, idx) + str + this.slice(idx + Math.abs(rem));
+};
+
+// execute regex
 function testRegEx(e) {
 	e.preventDefault();
 	
@@ -70,20 +78,36 @@ function testRegEx(e) {
 	// read textarea
 	var t = textInput.value;
 	var result = t;
+	
+	// set cookies
+	setCookie('rgx', rgxInput.value);
+	setCookie('args', args);
 
 	if (!rgxArgGlobal.checked) {
-
-		resNum.innerHTML = '- 1 match';
+		var o = 0; // offset
+		if ((match = t.match(r)) != null) {
+			result = result.insert(o + match.index, 0, iStart);
+			o+=iStart.length;
+			result = result.insert((o + match.index + match[0].length), 0, iEnd);
+			o+=iEnd.length;
+			resNum.innerHTML = '- match found';
+		}
+		else {
+			resNum.innerHTML = ' - no matches';
+		}
+		resRgx.innerHTML = r;
+		resDiv.innerHTML = result;
 		return true;
 	}
 	else {
-		var o = 0; // offest
+		var o = 0; // offset
 		var c = 0; // result counter
 		while ((match = r.exec(t)) != null) {
 			c++;
-			//console.log("match found at " + match.index +': '+ t.substring(match.index, match.index+match[0].length));
-			result = result.insert(o + match.index, 0, iStart); o+=8;
-			result = result.insert((o + match.index + match[0].length), 0, iEnd); o+=9;
+			result = result.insert(o + match.index, 0, iStart);
+			o+=iStart.length;
+			result = result.insert((o + match.index + match[0].length), 0, iEnd);
+			o+=iEnd.length;
 		}
 		
 		if(c==0) {
@@ -101,48 +125,98 @@ function testRegEx(e) {
 	
 }
 
-String.prototype.insert = function(idx, rem, str) {
-	return this.slice(0, idx) + str + this.slice(idx + Math.abs(rem));
-};
-
 // put Bacon Ipsum into the textarea
-function getBacon(e) {
-	e.preventDefault();
-	getBaconBtn.innerHTML = "serving bacon ... ";
+function getText() {
 	// create reader to get text from the response blob
 	var reader = new FileReader();
 	reader.onload = function() {
-		getBaconBtn.innerHTML = "Give me a new bacon";
 		textInput.value = reader.result;
 	}
 	// fetch paragraphs
-	fetch('https://baconipsum.com/api/?type=meat-and-filler&start-with-lorem=1&paras='+bacon+'&format=text')
-	.then(response => response.blob())
-	.then(blob => reader.readAsText(blob))
+	fetch('https://baconipsum.com/api/?type=meat-and-filler&start-with-lorem=1&paras='+setTextLen.value+'&format=text')
+		.then(response => response.blob())
+		.then(blob => reader.readAsText(blob));
+	setCookie('bacon', setTextLen.value);
+	setCookie('text', 'bacon');
 }
-function setBaconLength() {
-	bacon = setBaconLen.value;
-	if (bacon > 1) baconLenNum.innerHTML = bacon + " bacons";
-	else { baconLenNum.innerHTML = bacon + " bacons"; }
+function setTextLength() {
+	if (setTextLen.value > 1) inputTextLen.innerHTML = setTextLen.value + " bacons";
+	else { inputTextLen.innerHTML = setTextLen.value + " bacon"; }
+}
+// put expert text into the textarea
+function getExpertText() {
+	// create reader to get text from the response blob
+	var reader = new FileReader();
+	reader.onload = function() {
+		textInput.value = reader.result;
+	}
+	// fetch paragraphs
+	fetch(corsProxy + 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvprop=content&generator=random&grnnamespace=0&grnlimit=1')
+		.then(response => response.blob())
+		.then(blob => reader.readAsText(blob));
+	setCookie('bacon', setTextLen.value);
+	setCookie('text', 'expert');
 }
 
 function changeTheme() {
-	if(theme) {
+	if(!themeHref.href.match(/dark.css/)) {
 		themeHref.href = 'css/dark.css';
-		theme = false;
+		setCookie('theme', 'dark');
 	}
 	else {
 		themeHref.href = 'css/light.css';
-		theme = true;
+		setCookie('theme', 'light');
 	}
-	console.log('theme changed');
+}
+
+// cookie functions
+function setCookie(name,value,days) {
+	var expires = "";
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime() + (days*24*60*60*1000));
+		expires = "; expires=" + date.toUTCString();
+	}
+	document.cookie = name + "=" + value + expires + "; path=/";
+}
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+function eraseCookie(name) {
+	setCookie(name,"",-1);
+}
+
+// get cookies & set preferences
+function getPrefs() {
+	themeHref.href = 'css/' + readCookie('theme') + '.css';
+	setTextLen.value = readCookie('bacon');
+	rgxInput.value = readCookie('rgx');
+	if(readCookie('args').match(/g/)) rgxArgGlobal.checked = true;
+	if(readCookie('args').match(/i/)) rgxArgCase.checked = true;
+	if(readCookie('args').match(/m/)) rgxArgMultiLine.checked = true;
+	if(readCookie('args').match(/u/)) rgxArgUnicode.checked = true;
+	if(readCookie('text') == 'bacon') {
+		getText();
+	} else getExpertText();
 }
 
 // event listeners
-executeBtn.addEventListener('click', testRegEx);
-getBaconBtn.addEventListener('click', getBacon);
-setBaconLen.addEventListener('input', setBaconLength);
+getExpertTextBtn.addEventListener('click', getExpertText);
+getTextBtn.addEventListener('click', getText);
+setTextLen.addEventListener('input', setTextLength);
 changeThemeBtn.addEventListener('click', changeTheme);
+executeBtn.addEventListener('click', testRegEx);
+rgxInput.addEventListener("keypress", function(e) {
+	if (e.keyCode == 13)
+		testRegEx(e);
+});
 
 /*
 // test in console:
@@ -154,5 +228,6 @@ while ((match = re.exec(str)) != null) {
 */
 
 (function(){
-	setBaconLength();
+	getPrefs();
+	setTextLength();
 })();
